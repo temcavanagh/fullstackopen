6a1e63d13b2a17184bdd55047460bad944b13850
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-04-13 16:34:41 +1000

    Part 1(c)

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,67 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Parts = (props) => {
-  return (
-    <div>
-      <p>{props.parts.name} {props.parts.exercises}</p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Parts parts = {props.parts[0]}/>
-      <Parts parts = {props.parts[1]}/>
-      <Parts parts = {props.parts[2]}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  let sum = props.parts.reduce((tot,el) => tot + el.exercises, 0)
-
-  return (
-    <>
-      <p>Number of exercises {sum}</p>
-    </>
-  )
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
   }
-  
+
   return (
     <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
-} 
+}
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
-ReactDOM.render(<App />, document.getElementById('root')
-);
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text='left' />
+        <Button onClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
