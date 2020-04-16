477f85ebc7409b48ce8d8495d28513451dfcfafa
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-04-16 19:02:03 +1000

    1.9: unicafe step 4

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,51 +1,65 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
+const Statdisplay = ({ text, value }) => {
+  return(
+    <>
+      <tr>
+        <td>{text}: </td>
+        <td>{value}</td>
+      </tr>
+    </>
+  )
+}
+
+const Stats = ({good, neutral, bad}) => {
+  let all = (good + neutral + bad)
+  let avg = ((good - bad) / all)
+  let pos = ((good / all) * 100)
+
+  if (all === 0){
+    return(
       <div>
-        the app is used by pressing the buttons
+        <h2>Statistics</h2>
+        No feedback given
       </div>
     )
   }
 
-  return (
+  return(
     <div>
-      button press history: {props.allClicks.join(' ')}
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <Statdisplay text="Good" value={good}/>
+          <Statdisplay text="Neutral" value={neutral}/>
+          <Statdisplay text="Bad" value={bad}/>
+          <Statdisplay text="All" value={all}/>
+          <Statdisplay text="Average" value={avg}/>
+          <Statdisplay text="Positive" value={`${(pos)}%`}/>
+        </tbody>
+      </table>
     </div>
   )
 }
 
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-const App = (props) => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
 
   return (
     <div>
+      <h1>Give feedback</h1>
       <div>
-        {left}
-        <Button onClick={handleLeftClick} text='left' />
-        <Button onClick={handleRightClick} text='right' />
-        {right}
-        <History allClicks={allClicks} />
+        <button onClick={handleGood}>Good</button>
+        <button onClick={handleNeutral}>Neutral</button>
+        <button onClick={handleBad}>Bad</button>
+        <Stats good={good} neutral={neutral} bad={bad}/>
       </div>
     </div>
   )
