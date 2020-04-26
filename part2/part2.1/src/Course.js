f914f3da6295ee75cac9b031f63a1f800c942dd3
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-04-27 07:15:56 +1000

    Exercise 2.1

--- part2/part2.1/src/Course.js
+++ part2/part2.1/src/Course.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
+const Title = ({ title }) => <h1>{title}</h1>
+
+const Parts = ({ name, exercises }) => <p>{name}: {exercises}</p>
+
+const Content = ({ parts }) => parts.map(each => <Parts key={each.id} name={each.name} exercises={each.exercises}/>)
+
 const Course = ({ course }) => {
     return(
-        //Course
+        <div>
+            <Title title = {course.name}/>
+            <Content parts = {course.parts}/>
+        </div>
     )
 }
 
