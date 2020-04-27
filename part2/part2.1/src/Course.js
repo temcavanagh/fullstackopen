7db0ea27ce5749119dccba3d708b5d81dbe2b691
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-04-27 18:51:51 +1000

    Exercises 2.1 - 2.5 complete

--- part2/part2.1/src/Course.js
+++ part2/part2.1/src/Course.js
@@ -1,18 +1,23 @@
 import React from 'react'
 
-const Title = ({ title }) => <h1>{title}</h1>
+const Title = ({ title }) => <h2>{title}</h2>
 
 const Parts = ({ name, exercises }) => <p>{name}: {exercises}</p>
 
 const Content = ({ parts }) => parts.map(each => <Parts key={each.id} name={each.name} exercises={each.exercises}/>)
+
+const Sum = ({ parts }) => <p><strong>Total of {parts.reduce((sum, each) => sum + each.exercises, 0)} exercises</strong></p>
 
 const Course = ({ course }) => {
     return(
         <div>
             <Title title = {course.name}/>
             <Content parts = {course.parts}/>
+            <Sum parts = {course.parts}/>
         </div>
     )
 }
 
-export default Course
+const Curriculum = ({ courses }) => courses.map((each, index) => <Course key={each.id} course={courses[index]}/>)
+
+export default Curriculum
