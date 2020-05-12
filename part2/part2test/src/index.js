5444ea78aa808661b5cda185866f0c6c0d47643d
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-05-12 20:05:58 +1000

    part 2d

--- part2/part2test/src/index.js
+++ part2/part2test/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import axios from 'axios'
+
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+  })
 
 const notes = [
   {
@@ -23,7 +31,4 @@ const notes = [
   }
 ]
 
-ReactDOM.render(
-  <App notes={notes} />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
