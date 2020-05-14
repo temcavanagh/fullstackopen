984c928e0b0d6e4ee57654fd5caba7bdf7ba74b2
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-05-14 20:12:30 +1000

    Ex 2.11 Phonebook

--- part2/part2.6/src/App.js
+++ part2/part2.6/src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './components/filter.js';
 import Persons from './components/persons.js';
 import InputForm from './components/inputform.js';
+import axios from 'axios';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([])
   
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ filterNames, setFilterNames ] = useState('')
+
+  useEffect (() => {
+    console.log('effect')
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+            console.log('promise fulfilled')
+            setPersons(response.data)
+        })
+}, [])
+
+console.log('render', persons.length, 'notes')
 
   const addPerson = (event) => {
     event.preventDefault()
