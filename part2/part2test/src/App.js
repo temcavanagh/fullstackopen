83cc8e2e0e8e85582f2b53c1125529a82f032ac3
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-05-14 18:42:42 +1000

    Part 2d test app

--- part2/part2test/src/App.js
+++ part2/part2test/src/App.js
@@ -7,15 +7,16 @@ const App = ( props ) => {
     const [newNote, setNewNote] = useState('a new note...')
     const [showAll, setShowAll] = useState(true)
 
-    useEffect(() => {
+    useEffect (() => {
         console.log('effect')
         axios
             .get('http://localhost:3001/notes')
             .then(response => {
-            console.log('promise fulfilled')
-            setNotes(response.data)
+                console.log('promise fulfilled')
+                setNotes(response.data)
             })
     }, [])
+
     console.log('render', notes.length, 'notes')
 
     const addNote = (event) => {
