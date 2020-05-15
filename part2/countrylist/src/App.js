24ef01d182e29dd07f0233cb0d461e515d5a9e04
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-05-16 08:52:10 +1000

    countrylist app additions

--- part2/countrylist/src/App.js
+++ part2/countrylist/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
+const App = () => {
+
+  axios.get('https://restcountries.eu/rest/v2/all')
+  .then(response => {
+  const countries = response.data
+  console.log(countries)
+  })
+
+  const Filter = ({ filter, handleChange }) => {
+    return (
+      <div>
+          <form>
+            Find countries:
+            <input value={filter} onChange={handleChange}/>
+          </form>
+      </div>
+    )
+  }
+
+  return(
     <div>
-      Hello world
+      <Filter />
     </div>
-  );
+  )
 }
 
 export default App;
--- part2/countrylist/src/index.js
+++ part2/countrylist/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+axios.get('https://restcountries.eu/rest/v2/all')
+  .then(response => {
+  const countries = response.data
+  console.log(countries)
+  })
+  
+ReactDOM.render(<App />, document.getElementById('root'))
