17a55f4c488215629fb568e35e936618ee9eb081
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-05-18 19:42:14 +1000

    Ex 2.12 step 1 complete

--- part2/countrylist/src/App.js
+++ part2/countrylist/src/App.js
@@ -3,33 +3,66 @@ import axios from 'axios';
 
 const App = () => {
 
-  const [ countryList, setCountryList ] = useState([])
-  const [ search, setSearch] = useState('')
-  const [ showCountry, setShowCountry] = useState(null)
+  const [ countryList, setCountryList ] = useState([]);
+  const [ search, setSearch] = useState('');
+  const [ showCountry, setShowCountry] = useState({});
 
-  const handleSearch = (event) => {setCountryList(event.target.value)}
+  const handleSearch = event => {
+    setSearch(event.target.value); 
+    setShowCountry({})
+  }
+  
+  const handleShowCountry = country => {
+    setShowCountry({ ...country })
+  }
 
-  const hook = () => {
-    const afterData = ( countryData ) => {
-      setSearch(countryData.data);
-    };
-    axios.get('https://restcountries.eu/rest/v2/all').then(afterData);
-  };
-  useEffect(hook, []);
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(res => {setCountryList(res.data)})
+  }, []);
+  
+  const displayCountries = () => {
+    const filterCountries = countryList.filter(country => {
+      return country.name.toLowerCase().includes(search);
+    })
+    if (filterCountries.length > 30 ) return '';
+    if (filterCountries.length > 10) {
+      return <li>Over ten matches</li>;
+    } else {
+      return filterCountries.map(country => (
+        <div key={country.name}>
+          <div>{country.name}</div>
+          <button onClick={() => handleShowCountry(country)}>Show country</button> 
+        </div>
+      ))
+    }
+  } 
 
-  // Export Filter as component
-
-  const Filter = ({ countryName, handleChange}) => {
+  const displaySingleCountry = () => {
+    const languages = showCountry.languages.map(language => {
+      return <li key={language.name}>{language.name}</li>
+    })
     return (
       <div>
-        {countryName}: <input onChange={handleChange}></input>
+        <h2>{showCountry.name}</h2>
+        <p>Capital: {showCountry.capital}</p>
+        <p>Population: {showCountry.population}</p>
+        <p>Languages: </p>
+        <ul>{languages}</ul>
+        <img src={showCountry.flag} alt="flag" width={250} height={175} />
       </div>
     )
   }
 
-  return(
+  return (
     <div>
-      <Filter countryName="Find countries" onChange={handleSearch} />
+      <div>
+        <div>Find countries:</div>
+        <input onChange={handleSearch} value={search} />
+      </div>
+      {countryList.length && displayCountries()}
+      {showCountry.name && displaySingleCountry()}
     </div>
   )
 }
