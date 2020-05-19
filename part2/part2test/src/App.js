8d024ac752ae55cd2589b4b586c3940819a7a59b
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-05-20 07:38:23 +1000

    Part 2d notes and notes app wip

--- part2/part2test/src/App.js
+++ part2/part2test/src/App.js
@@ -25,10 +25,15 @@ const App = ( props ) => {
             content: newNote,
             date: new Date().toISOString(),
             important: Math.random() < 0.5,
-            id: notes.length + 1,
+            //id: notes.length + 1,
         }
-        setNotes(notes.concat(noteObject))
-        setNewNote('')                            
+
+        axios
+            .post('http://localhost:3001/notes', noteObject)
+            .then(response => {
+                setNotes(notes.concat(response.data))
+                setNewNote('')                            
+            })                           
       }
     
     const handleNoteChange = (event) => {
