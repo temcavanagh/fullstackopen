e663536b984e5a73a6beba7d194b3897217eee35
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-05-20 20:16:53 +1000

    Part 2d content wip

--- part2/part2test/src/App.js
+++ part2/part2test/src/App.js
@@ -45,6 +45,16 @@ const App = ( props ) => {
         ? notes
         : notes.filter(note => note.important === true)
 
+    const toggleImportanceOf = (id) => {
+        const url = `http://localhost:3001/notes/${id}`
+        const note = notes.find(n => n.id === id)
+        const changedNote = { ...note, important: !note.important }
+
+        axios.put(url, changedNote).then(response => {
+            setNotes(notes.map(note => note.id !== id ? note : response.data))
+        })
+    }
+
     return(
         <div>
             <h1>Notes</h1>
@@ -55,7 +65,11 @@ const App = ( props ) => {
             </div>
             <ul>                                            {/* Display notes list */}
                 {notesToShow.map(note =>
-                    <Note key={note.id} note={note} />
+                    <Note 
+                        key={note.id} 
+                        note={note}
+                        toggleImportance={() => toggleImportanceOf(note.id)}
+                    />
                 )}
             </ul>
             <form onSubmit={addNote}>                       {/* addNote function as event handler */}
