ae254469511081d4a9e9c98020c4e7e0c4e5b661
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-05-27 18:23:03 +1000

    part 2e wip

--- part2/part2test/src/App.js
+++ part2/part2test/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
 
 const App = ( props ) => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('a new note...')
     const [showAll, setShowAll] = useState(true)
+    const [errorMessage, setErrorMessage] = useState('some error happened...')
 
     useEffect (() => {
         noteService
@@ -52,9 +53,12 @@ const App = ( props ) => {
                 setNotes(notes.map(note => note.id !== id ? note : returnedNote))
             })
             .catch(error => {
-                alert(
+                setErrorMessage(
                     `The note '${note.content}' was already deleted from the server`
                 )
+                setTimeout(() => {
+                    setErrorMessage(null)
+                }, 5000)
                 setNotes(notes.filter(n => n.id !== id))
             })
     }
@@ -62,6 +66,7 @@ const App = ( props ) => {
     return(
         <div>
             <h1>Notes</h1>
+            <Notification message={errorMessage} />
             <div>                                           {/* Show important */}
                 <button onClick={() => setShowAll(!showAll)}>
                     show {showAll ? 'important' : 'all' }
