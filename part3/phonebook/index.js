8a91b8f1d8897e33831b44a90cd96141781b5ce5
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-06-02 18:37:30 +1000

    ex 3.2 complete

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -30,6 +30,13 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (req, res) => {
+    res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `)
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
