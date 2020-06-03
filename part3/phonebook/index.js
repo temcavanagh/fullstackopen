538f7c9a1edaab0020a74408320286e3b4eb9105
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-06-03 17:35:47 +1000

    phonebook exercises 3.7 & 3.8 complete

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+app.use(express.static('build'))
+morgan.token('body', function (req, res) {
+    return JSON.stringify(req.body)
+})
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
     {
