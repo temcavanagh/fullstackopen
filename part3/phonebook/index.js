dd88977ec32d32dd5301d47f19a0c6b2ba1caed0
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-06-04 18:49:59 +1000

    part 3b coursework

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 app.use(express.static('build'))
 morgan.token('body', function (req, res) {
