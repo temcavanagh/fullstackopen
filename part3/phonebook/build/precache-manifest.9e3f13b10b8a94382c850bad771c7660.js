bb483446de352f5aea7d33510758595e296f56f9
Temuera Cavanagh <cavanaghtd@gmail.com>
2020-06-29 19:11:17 +1000

    ex 3.10 production build

---
+++ part3/phonebook/build/precache-manifest.9e3f13b10b8a94382c850bad771c7660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9ed7ee25c700e6c4b24873442214ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2026817a71839bd5374b",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "1ce6afb0b64031ef9b71",
+    "url": "/static/js/2.c625c379.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c625c379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2026817a71839bd5374b",
+    "url": "/static/js/main.323c5234.chunk.js"
+  },
+  {
+    "revision": "ba306c866d5784bb2ded",
+    "url": "/static/js/runtime-main.39c52c65.js"
+  }
+]);
